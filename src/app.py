464e9700f56be88698be419e39b567458846c951
kyrie0126 <kylewong00@gmail.com>
2025-06-12 22:21:55 -0400

    make dictionary downloadable for testing

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from model import random_guess
-from utils import csv_to_list
+from utils import csv_to_list, dict_to_csv
 
 st.title("Domain Classifier")
 st.subheader('MIDS Capstone Project')
@@ -11,7 +11,10 @@ file_upload = st.file_uploader('Upload a single column of domains', type='csv')
 if file_upload:
     domain_list = csv_to_list(file_upload)
     guesses = random_guess(domain_list)
-    st.write(guesses)
+    output_file = dict_to_csv(data=guesses)
+    
+    st.download_button('Download results', output_file, file_name = f'{file_upload.name}_predictions.csv')
+    # st.write(guesses)
     
 else:
     st.write("Please upload a csv of domains")
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
 import csv
+import io
 
 def csv_to_list(uploaded_file):
-    '''converts uploaded csv to a list'''
+    """Convert csv to list"""
     file_text = uploaded_file.read().decode("utf-8").splitlines()
     reader = csv.DictReader(file_text)
 
@@ -9,3 +10,12 @@ def csv_to_list(uploaded_file):
     domains = [row["domain"] for row in reader if "domain" in row and row["domain"].strip() != ""]
 
     return domains
+
+
+def dict_to_csv(data):
+    """Convert list of dictionaries to CSV in memory"""
+    output = io.StringIO()
+    writer = csv.DictWriter(output, fieldnames=data[0].keys())
+    writer.writeheader()
+    writer.writerows(data)
+    return output.getvalue()
